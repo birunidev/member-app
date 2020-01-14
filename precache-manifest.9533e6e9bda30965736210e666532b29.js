6396a069e4ef472c10ccd1457051803bce655cb8
unknown <birunimamak@gmail.com>
2020-01-14 19:05:30 +0700

    Updates

---
+++ precache-manifest.9533e6e9bda30965736210e666532b29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6417d5b63d2a02fff33b067945db4ea9",
+    "url": "/member-app/index.html"
+  },
+  {
+    "revision": "b7e0e4376259cdfbe0d7",
+    "url": "/member-app/static/css/main.1cfdd782.chunk.css"
+  },
+  {
+    "revision": "546197b248812e1a707b",
+    "url": "/member-app/static/js/2.f9552265.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/member-app/static/js/2.f9552265.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7e0e4376259cdfbe0d7",
+    "url": "/member-app/static/js/main.dde8dc84.chunk.js"
+  },
+  {
+    "revision": "4d40f4015a4f61446ac0",
+    "url": "/member-app/static/js/runtime-main.07f5f925.js"
+  },
+  {
+    "revision": "6a4886d112995431b77fcd8e512c088f",
+    "url": "/member-app/static/media/back.6a4886d1.svg"
+  },
+  {
+    "revision": "1000c9e20d27201d2d7c259764e04697",
+    "url": "/member-app/static/media/whatsapp.1000c9e2.svg"
+  }
+]);
